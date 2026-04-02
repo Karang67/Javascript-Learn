1372415cd6c193dba310c1ad8afff6574aea58c6
karan gehlot <124277228+Karang67@users.noreply.github.com>
2026-04-02 13:31:38 +0000

    let, const, and var

---
+++ 01basics/01Variables.js
@@ -0,0 +1,9 @@
+const accountId = 14654;
+let accountEmial = "Karan@gamial.com"
+var accountpassword = "12345"
+accountCity = "Jaipur"
+
+accountEmial = "hc@hc.com"
+accountpassword = "2121"
+
+console.log([accountId,accountEmial,accountpassword,accountCity])
