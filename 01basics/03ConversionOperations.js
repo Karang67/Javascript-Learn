f06923491dcf1644a261218c7db6b022a7f7c105
karan gehlot <124277228+Karang67@users.noreply.github.com>
2026-04-02 13:48:06 +0000

    Conversion

---
+++ 01basics/03ConversionOperations.js
@@ -0,0 +1,26 @@
+let score = "33"
+
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+
+
+//"33" => 33
+//"33abc" => NaN
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 => true; 0 => false
+//"" => false
+//"Name" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
