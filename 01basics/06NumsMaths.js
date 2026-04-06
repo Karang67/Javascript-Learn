11918814ca98642cb117718f5eb4512d8b2f2646
karan gehlot <124277228+Karang67@users.noreply.github.com>
2026-04-06 12:30:59 +0000

    Math and Numms

---
+++ 01basics/06NumsMaths.js
@@ -0,0 +1,39 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+// const otherNumber = 123.89798
+// console.log(otherNumber.toPrecision(3))
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString())
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+
+// ++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4))
+// console.log("Round Value " + Math.round(4.6))
+// console.log("Ceiling Value " + Math.ceil(4.2))
+// console.log("Floor Value " + Math.floor(4.9))
+// console.log("minimum Value " + Math.min(4,5,3,2,6,2,7))
+
+
+console.log("maximum Value " + Math.random()) // greater than 0 or less than 1 (Generate random value)
+
+console.log((Math.random()*6) + 1) 
+console.log(Math.ceil(Math.random()*6)) 
+console.log(Math.floor(Math.random()*6)) 
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min)) 
