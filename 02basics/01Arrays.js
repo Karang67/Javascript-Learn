5f52ba86e97e932d27e4c9deef3ec2e42507012a
karan gehlot <124277228+Karang67@users.noreply.github.com>
2026-04-07 09:52:38 +0000

    Arrays

---
+++ 02basics/01Arrays.js
@@ -0,0 +1,44 @@
+// console.log("Arrays...")
+
+// const myArr = [0, 1, 2, 3, 4, 5]
+// const myHeroes = ["Saktiman", "IronMan"]
+
+// const Arrays2 = new Array(0,1,2,3,4,5)
+// console.log(myArr[0])
+// console.log(myHeroes[0])
+// console.log(Arrays2[0])
+
+
+// Array Methods
+
+const myArray = [0, 1, 2, 3, 4, 5]
+
+// myArray.push(6)
+// myArray.push(7)
+// myArray.pop()
+
+// myArray.unshift(9)
+// myArray.shift()
+
+
+
+// console.log(myArray.includes(9))
+// console.log(myArray.indexOf(3))
+
+// const newArr = myArray.join()
+// console.log(myArray)
+// console.log(newArr)
+
+// Slice and Splice
+
+console.log("A", myArray)
+
+const myn1 = myArray.slice(1,3)
+
+console.log(myn1)
+console.log("B",myArray)
+
+const myn2 = myArray.splice(1,3)
+
+console.log("C",myArray)
+console.log(myn2)
